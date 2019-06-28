ef477ed97832cc64920c62bbbc1758617d3c7498
Daniel Caldas <daniel@dcaldas.rj.ip.tv>
2019-06-28 16:00:08 -0300

    webView

--- app/src/main/java/adroidproject/darlan/ucam/com/a18horas/MainActivity.java
+++ app/src/main/java/adroidproject/darlan/ucam/com/a18horas/MainActivity.java
@@ -1,5 +1,7 @@
 package adroidproject.darlan.ucam.com.a18horas;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        webView = findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        webView.loadUrl("https://manoweb.ip.tv/iframe/mixmanaus");
+
     }
 
     @Override
@@ -37,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+
+
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/adroidproject/darlan/ucam/com/a18horas/SplashActivity.java
+++ app/src/main/java/adroidproject/darlan/ucam/com/a18horas/SplashActivity.java
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(getBaseContext(), MainActivity.class));
                 finish();
             }
-        }, 3000);
+        }, 2000);
     }
 }
